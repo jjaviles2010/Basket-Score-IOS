eeb7bc86405bebcbb737e10677d7492f447c1121
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-08-18 22:41:47 -0300

    Creating Actions to add Score to teams.

--- Basket Score/ScoreViewController.swift
+++ Basket Score/ScoreViewController.swift
@@ -13,6 +13,18 @@ class ScoreViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var lbTime1: UILabel!
     @IBOutlet weak var lbTime2: UILabel!
+    @IBOutlet weak var tfScoreTeam1: UITextField!
+    @IBOutlet weak var tfScoreTeam2: UITextField!
+    
+    @IBOutlet weak var btnOneTime1: UIButton!
+    @IBOutlet weak var btnTwoTime1: UIButton!
+    @IBOutlet weak var btnThreeTime1: UIButton!
+    
+    @IBOutlet weak var btnOneTime2: UIButton!
+    @IBOutlet weak var btnTwoTime2: UIButton!
+    @IBOutlet weak var btnThreeTime2: UIButton!
+    
+    @IBOutlet weak var tvMatches: UITextView!
     
     // MARK: - Properties
     var time1: String = ""
@@ -24,6 +36,48 @@ class ScoreViewController: UIViewController {
         
         lbTime1.text = time1
         lbTime2.text = time2
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func addScore(_ sender: UIButton) {
+        var pointsTeam1 = 0
+        var pointsTeam2 = 0
+        
+        switch sender {
+        case btnOneTime1:
+            pointsTeam1 += 1
+        case btnTwoTime1:
+            pointsTeam1 += 2
+        case btnThreeTime1:
+            pointsTeam1 += 3
+        case btnOneTime2:
+            pointsTeam2 += 1
+        case btnTwoTime2:
+            pointsTeam2 += 2
+        case btnThreeTime2:
+            pointsTeam2 += 3
+        default:
+            break;
+        }
+        
+        if(pointsTeam1 > 0){
+            var scoreTeam1 = Int(tfScoreTeam1!.text!) ?? 0
+            tfScoreTeam1.text = "\(scoreTeam1 + pointsTeam1)"
+        }
+        
+        if(pointsTeam2 > 0){
+            var scoreTeam2 = Int(tfScoreTeam2!.text!) ?? 0
+            tfScoreTeam2.text = "\(scoreTeam2 + pointsTeam2)"
+        }
+        
+    }
+    
+    @IBAction func registerMatch(_ sender: UIButton) {
+        tvMatches.text += "\(time1) (\(tfScoreTeam1.text!)) X (\(tfScoreTeam2.text!)) \(time2)\n"
+        
+        tfScoreTeam1.text = "0"
+        tfScoreTeam2.text = "0"
     }
     
 
